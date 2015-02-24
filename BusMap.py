10101895e75c32825828c817eccd197e653405ed
gmontoya915 <gabrielmont915@gmail.com>
2015-02-23 17:16:59 -0800

    Update BusMap.py

--- BusMap.py
+++ BusMap.py
@@ -1,6 +1,40 @@
 import json
 import urllib2
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_mode([640,480])
+white = [255, 255, 255]
 
+
+def update_map():
+    api = "https://publicdata-transit.firebaseio.com/sf-muni/vehicles.json"
+    data = get_data(api)
+
+    # for routes that are just numbers the input parameter 
+    # should be int instead of string
+    number = 'N'
+    location = find_bus(number, data)
+
+    latitude = location['lat']
+    longitude = location['lon']
+
+    map_url = get_map(latitude, longitude)
+    #save image
+    map_image = urllib2.urlopen(map_url).read()
+    output = open("file01.png","wb")
+    output.write(map_image)
+    output.close()
+    #load the image
+    pic = pygame.image.load('file01.png')
+
+
+    #Open picture in pygame
+    screen.blit(pic,[0,0])
+
+    #update the display
+    pygame.display.update()
+
+    
 def get_data(api):
     get = urllib2.urlopen(api).read()
     data = json.loads(get)
@@ -26,18 +60,6 @@ def find_address(address):
 
 
 
-api = "https://publicdata-transit.firebaseio.com/sf-muni/vehicles.json"
-data = get_data(api)
-
-# for routes that are just numbers the input parameter 
-# should be int instead of string
-number = 'N'
-location = find_bus(number, data)
-
-latitude = location['lat']
-longitude = location['lon']
-
-map_url = get_map(latitude, longitude)
 
 address = "333+Jefferson+Street+San+Francisco+CA"
 location = find_address(address)
@@ -48,11 +70,34 @@ longitude = location['lng']
 
 map_url2 = get_map(latitude, longitude)
 
-#save image
-map_image = urllib2.urlopen(map_url).read()
-output = open("file01.png","wb")
-output.write(map_image)
-output.close()
+
+
+
+
+running = True 
+
+    
+
+while running:
+    update_map()
+    pygame.time.delay(5000)
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+#update the display
+pygame.display.update()
+pygame.quit()
+
+
+
+
+
+
+
 
 
 
